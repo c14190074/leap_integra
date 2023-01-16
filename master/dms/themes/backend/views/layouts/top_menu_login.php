89f3b889aa1d48d6d914081d0f98c3640936ea43
agungwibowo <c14190074@john.petra.ac.id>
2023-01-16 13:45:28 +0700

    memperbaiki link pada top menu

--- master/dms/themes/backend/views/layouts/top_menu_login.php
+++ master/dms/themes/backend/views/layouts/top_menu_login.php
@@ -4,7 +4,7 @@
       <!-- Navbar -->
       <nav class="navbar navbar-expand-lg blur blur-rounded top-0 z-index-3 shadow position-absolute my-3 py-2 start-0 end-0 mx-4">
         <div class="container-fluid pe-0">
-          <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 " href="../pages/dashboard.html">
+          <a class="navbar-brand font-weight-bolder ms-lg-0 ms-3 " href="<?= Snl::app()->baseUrl() ?>admin/dashboard/index">
             ITS | Document Management System
           </a>
           <button class="navbar-toggler shadow-none ms-2" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,25 +17,25 @@
           <div class="collapse navbar-collapse" id="navigation">
             <ul class="navbar-nav mx-auto ms-xl-auto me-xl-7">
               <li class="nav-item">
-                <a class="nav-link d-flex align-items-center me-2 active" aria-current="page" href="../pages/dashboard.html">
+                <a class="nav-link d-flex align-items-center me-2 active" aria-current="page" href="<?= Snl::app()->baseUrl() ?>admin/dashboard/index">
                   <i class="fa fa-chart-pie opacity-6 text-dark me-1"></i>
                   Dashboard
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link me-2" href="../pages/profile.html">
+                <a class="nav-link me-2" href="#">
                   <i class="fa fa-user opacity-6 text-dark me-1"></i>
                   Profile
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link me-2" href="../pages/sign-up.html">
+                <a class="nav-link me-2" href="<?= Snl::app()->baseUrl() ?>admin/user/register">
                   <i class="fas fa-user-circle opacity-6 text-dark me-1"></i>
                   Sign Up
                 </a>
               </li>
               <li class="nav-item">
-                <a class="nav-link me-2" href="../pages/sign-in.html">
+                <a class="nav-link me-2" href="<?= Snl::app()->baseUrl() ?>admin/user/login">
                   <i class="fas fa-key opacity-6 text-dark me-1"></i>
                   Sign In
                 </a>
