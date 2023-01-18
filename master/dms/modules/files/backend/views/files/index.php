325c501d7950992c4c22da27ef6276cf24fda806
agungwibowo <c14190074@john.petra.ac.id>
2023-01-18 11:26:13 +0700

    membuat tampilan untuk daftar folder

--- master/dms/modules/files/backend/views/files/index.php
+++ master/dms/modules/files/backend/views/files/index.php
@@ -3,10 +3,51 @@
     echo Snl::app()->getFlashMessage();
 ?>
 
-<div class="row">
+<div class="card">
+  <div class="table-responsive">
+    <table class="table align-items-center mb-0">
+      <thead>
+        <tr>
+          <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Folder</th>
+          <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Deskripsi</th>
+          <th class="text-uppercase text-secondary text-xxs font-weight-bolder opacity-7 ps-2">Owner</th>
+          <th class="text-center text-uppercase text-secondary text-xxs font-weight-bolder opacity-7">Last Updated</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php for($i=0; $i<5; $i++) : ?>
+            <tr>
+              <td>
+                <div class="d-flex px-2 py-1">
+                  <div>
+                    <!-- <img src="https://demos.creative-tim.com/soft-ui-design-system-pro/assets/img/team-2.jpg" class="avatar avatar-sm me-3"> -->
+                    <i class="fa fa-folder opacity-6 text-dark me-3"></i>
+                  </div>
+                  <div class="d-flex flex-column justify-content-center">
+                    <h6 class="mb-0 text-secondary text-sm text-dark">Leap UK. Petra</h6>
+                  </div>
+                </div>
+              </td>
+
+              <td>
+                <p class="text-xs text-secondary mb-0">Dokumen-dokumen program leap (agung wibowo)</p>
+              </td>
+
+              <td>
+                <p class="text-xs font-weight-bold mb-0">Mas Agung</p>
+                <p class="text-xs text-secondary mb-0">mas@agung.com</p>
+              </td>
+
+              <td class="align-middle text-center text-sm">
+                <p class="text-xs text-secondary mb-0">18 Jan 2023 11:00</p>
+              </td>
+            </tr>
+        <?php endfor; ?>
+        
+      </tbody>
+    </table>
+  </div>
 </div>
-
-
 
 <script type="text/javascript">
     $(document).ready(function() {
--- master/dms/themes/backend/views/layouts/left_menu.php
+++ master/dms/themes/backend/views/layouts/left_menu.php
@@ -1,7 +1,7 @@
 <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 " id="sidenav-main">
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-      <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/soft-ui-dashboard/pages/dashboard.html " target="_blank">
+      <a class="navbar-brand m-0" href="<?= Snl::app()->baseUrl() ?>admin/dashboard/index" target="_blank">
         <img src="<?= Snl::app()->config()->theme_url ?>assets_soft/img/logo-ct-dark.png" class="navbar-brand-img h-100" alt="main_logo">
         <span class="ms-1 font-weight-bold">ITS | DMS</span>
       </a>
@@ -10,7 +10,7 @@
     <div class="collapse navbar-collapse  w-auto " id="sidenav-collapse-main">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link  active" href="<?= Snl::app()->baseUrl() ?>admin/dashboard/index">
+          <a class="nav-link<?= $module == "dashboard" ? " active" : ""; ?>" href="<?= Snl::app()->baseUrl() ?>admin/dashboard/index">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
               <svg width="12px" height="12px" viewBox="0 0 45 40" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                 <title>shop </title>
@@ -31,9 +31,9 @@
         </li>
 
         <li class="nav-item">
-          <a class="nav-link" href="<?= Snl::app()->baseUrl() ?>admin/files/index">
+          <a class="nav-link<?= $module == "files" ? " active" : ""; ?>" href="<?= Snl::app()->baseUrl() ?>admin/files/index">
             <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
-              <i class="fa fa-folder opacity-6 text-dark"></i>
+              <i class="fa fa-folder-o opacity-6 <?= $module == "files" ? " text-white" : "text-dark"; ?>"></i>
             </div>
             <span class="nav-link-text ms-1">Files</span>
           </a>
