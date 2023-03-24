5e6efc253bb67bdc774ba3ebb2c4f7d105333220
c14190074 <c14190074@john.petra.ac.id>
2023-03-24 09:12:43 +0700

    membuat api untuk get shared folder

--- master/dms/modules/files/api/controllers/FilesController.php
+++ master/dms/modules/files/api/controllers/FilesController.php
@@ -306,54 +306,59 @@
 
 					}
 
-					
-					// if($model != NULL) {
-					// 	foreach($model as $folder) {
-					// 		if($folder->hasAccess($this->user_id)) {
-					// 			$user_created = User::model()->findByPk($folder->created_by);
-					//         	$user_updated = User::model()->findByPk($folder->updated_by);
-					//         	$user_access_string = '';
 
-					//         	if($folder->user_access != NULL) {
-					//                 $user_email = array();
-					//                 $user_access = json_decode($folder->user_access);
-					//                 foreach($user_access as $d) {
-					//                   $user_access_model = User::model()->findByPk($d->user);
-					//                   if($folder->type == "file") {
-					//                     $tmp_str = $user_access_model->email . "(".implode(',', $d->role).")";
-					//                     array_push($user_email, $tmp_str);
-					//                   } else {
-					//                     array_push($user_email, $user_access_model->email);
-					//                   }
-					                  
-					//                 }
+					$result = array(
+						'status' => 200,
+						'total_data' => count($data),
+						'data'	 => $data,
+					);
 
-					//                 array_push($user_email, $user_created->email." (owner)");
-					//                 $user_access_string = implode( ", ", $user_email);
-					//               } 
+					$this->renderJSON($result);
+				} else {
+					$this->renderErrorMessage(405, 'MethodNotAllowed');
+				}
+			} else {
+				$this->renderInvalidUserToken();
+			}
+		}
 
-					//         	$data[] = array(
-					// 				'folder_id' 		=> $folder->folder_id,
-					// 				'folder_parent_id' 	=> $folder->folder_parent_id,
-					// 				'name' 		=> $folder->name,
-					// 				'nomor' 	=> $folder->nomor,
-					// 				'perihal' 	=> $folder->perihal,
-					// 				'type' 		=> ucwords(strtolower($folder->type)),
-					// 				'format' 	=> $folder->format,
-					// 				'size' 		=> $folder->size,
-					// 				'description' 	=> $folder->description,
-					// 				'created_by' 	=> ucwords(strtolower($user_created->fullname)),
-					// 				'created_on' 	=> date('d M Y H:i:s', strtotime($folder->created_on)),
-					// 				'updated_on' 	=> date('d M Y H:i:s', strtotime($folder->updated_on)),
-					// 				'updated_by' 	=> ucwords(strtolower($user_updated->fullname)),
-					// 				'user_access'	=> $user_access_string,
-					// 				//'related_document' => implode(', ', $folder->getRelatedDocuments()),
-					// 			);
+		public function getsharedfolder() {
+			if($this->valid_user_token) {
+				if($this->request_type == 'GET') {
+					$shared_folders = NULL;
+					$data = array();
 
-					// 		}
-					// 	}
-					// } 
+					$shared_folders = Folder::model()->findAll(array(
+						'condition' => 'is_deleted = 0 AND type = "folder" AND user_access IS NOT NULL AND created_by != :id',
+						'params'	=> array(':id' => $this->user_id)
+					));
 
+
+					if($shared_folders != NULL && Folder::hasSharedFolder($this->user_id)) {
+						foreach($shared_folders as $folder) {
+							if($folder->hasAccess($this->user_id)) {
+								$folder_created = User::model()->findByPk($folder->created_by);
+                            	$data[] = array(
+									'folder_id' 		=> $folder->folder_id,
+									'folder_parent_id' 	=> $folder->folder_parent_id,
+									'name' 		=> $folder->name,
+									'nomor' 	=> $folder->nomor,
+									'perihal' 	=> $folder->perihal,
+									'type' 		=> ucwords(strtolower($folder->type)),
+									'format' 	=> $folder->format,
+									'size' 		=> $folder->size,
+									'description' 	=> $folder->description,
+									'created_by' 	=> ucwords(strtolower($folder_created->fullname)),
+									'created_on' 	=> date('d M Y H:i:s', strtotime($folder->created_on)),
+									'updated_on' 	=> date('d M Y H:i:s', strtotime($folder->updated_on)),
+									// 'updated_by' 	=> ucwords(strtolower($user_updated->fullname)),
+									// 'user_access'	=> $user_access_string,
+									//'related_document' => implode(', ', $folder->getRelatedDocuments()),
+								);
+							}
+						}
+
+					}
 
 					$result = array(
 						'status' => 200,
